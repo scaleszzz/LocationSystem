6b854b51a49653278ac061aec27d66df23b9a333
Aibar <aibar03aibar@gmail.com>
2024-08-17 11:06:43 +0500

    add UserService

---
+++ src/main/java/com/example/locationsystem/service/UserService.java
@@ -0,0 +1,30 @@
+package com.example.locationsystem.service;
+
+import com.example.locationsystem.model.User;
+import com.example.locationsystem.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private UserRepository userRepository;
+
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public Optional<User> getUserById(Long id) {
+        return userRepository.findById(id);
+    }
+
+}
